98cebbf84b07f921feeed5dd65d367008193e328
Thawngte <Thawngte@administrators-iMac-3.local>
2021-12-11 09:02:07 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import Home from './components/Home';
+import Login from './components/Login';
+import Profile from './components/Profile';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className='App'>
+			<Router>
+				<Routes>
+					<Route exact path='/' element={<Home />} />
+					<Route exact path='/login' element={<Login />} />
+					<Route exact path='/profile' element={<Profile />} />
+				</Routes>
+			</Router>
+		</div>
+	);
 }
 
 export default App;
